ae83c25b02ffbfb6d5a000eeb69b5ea54c8a8999
Tal Spektor <tal.s@zemingo.com>
2022-04-30 15:35:09 +0300

    Initial Commit

---
+++ LineGraphDemo/LineGraphDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineGraphDemoApp.swift
+//  LineGraphDemo
+//
+//  Created by Tal talspektor on 30/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LineGraphDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
